acdc8a13a368a8bf6add06b433d6d2ed0946d143
yanghou2000 <a13008878185@gmail.com>
2022-11-28 16:21:34 +0000

    first commit

---
+++ Modules.py
@@ -0,0 +1,39 @@
+# This file contains GlobalSAModule, SAModule
+
+import os
+import torch
+import torch.nn.functional as F
+
+import torch_geometric.transforms as T
+from torch_geometric.nn import MLP, PointConv, fps, global_max_pool, radius
+
+class SAModule(torch.nn.Module):
+    '''Set Abstraction Module
+    '''
+    def __init__(self, ratio, r, nn):
+        super().__init__()
+        self.ratio = ratio  #ratio of points to be sampled
+        self.r = r  #radius of circle in which points are grouped
+        self.conv = PointConv(nn, add_self_loops=False)
+
+    def forward(self, x, pos, batch):
+        idx = fps(pos, batch, ratio=self.ratio)
+        row, col = radius(pos, pos[idx], self.r, batch, batch[idx],
+                          max_num_neighbors=64)
+        edge_index = torch.stack([col, row], dim=0)
+        x_dst = None if x is None else x[idx]
+        x = self.conv((x, x_dst), (pos, pos[idx]), edge_index)
+        pos, batch = pos[idx], batch[idx]
+        return x, pos, batch
+
+class GlobalSAModule(torch.nn.Module):
+    def __init__(self, nn):
+        super().__init__()
+        self.nn = nn
+
+    def forward(self, x, pos, batch):
+        x = self.nn(torch.cat([x, pos], dim=1))
+        x = global_max_pool(x, batch)
+        pos = pos.new_zeros((x.size(0), 3))
+        batch = torch.arange(x.size(0), device=batch.device)
+        return x, pos, batch
---
+++ PointNet2_segmentation.py
@@ -0,0 +1,156 @@
+import os
+import yaml
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from Modules import GlobalSAModule, SAModule
+from torch_scatter import scatter
+from torchmetrics.functional import jaccard_index
+
+import torch_geometric.transforms as T
+from Semantic_Dataloader import SemanticKittiGraph
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import MLP, knn_interpolate
+from torch_geometric.data import Dataset, Data
+
+
+data_path = '/Volumes/scratchdata/kitti/dataset/'
+DATA_path = '/home/yanghou/project/PointNet++/semantic-kitti.yaml'
+
+testing_squences = ['00']
+
+train_sequences = ['00', '01', '02', '03', '04', '05', '06']
+val_sequences = ['07']
+test_sequences = ['08', '09', '10']
+
+train_dataset = SemanticKittiGraph(dataset_dir='/Volumes/scratchdata/kitti/dataset/', 
+                                sequences= testing_squences, 
+                                DATA_dir=DATA_path)
+
+test_dataset = SemanticKittiGraph(dataset_dir='/Volumes/scratchdata/kitti/dataset/', 
+                                sequences= val_sequences, 
+                                DATA_dir=DATA_path)
+
+torch.manual_seed(42)
+train_loader = DataLoader(train_dataset, batch_size=12, shuffle=True, num_workers=6)
+test_loaer = DataLoader(test_dataset, batch_size=12, shuffle=False, num_workers=6)
+
+# add loss weights beforehand
+loss_w = train_dataset.map_loss_weight()
+
+class FPModule(torch.nn.Module):
+    def __init__(self, k, nn):
+        super().__init__()
+        self.k = k
+        self.nn = nn
+
+    def forward(self, x, pos, batch, x_skip, pos_skip, batch_skip):
+        x = knn_interpolate(x, pos, pos_skip, batch, batch_skip, k=self.k)
+        if x_skip is not None:
+            x = torch.cat([x, x_skip], dim=1)
+        x = self.nn(x)
+        return x, pos_skip, batch_skip
+
+
+class Net(torch.nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        # Input channels account for both `pos` and node features.
+        self.sa1_module = SAModule(0.2, 0.2, MLP([3 + 3, 64, 64, 128]))
+        self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
+        self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
+
+        self.fp3_module = FPModule(1, MLP([1024 + 256, 256, 256]))
+        self.fp2_module = FPModule(3, MLP([256 + 128, 256, 128]))
+        self.fp1_module = FPModule(3, MLP([128 + 3, 128, 128, 128]))
+
+        self.mlp = MLP([128, 128, 128, num_classes], dropout=0.5, norm=None)
+
+        self.lin1 = torch.nn.Linear(128, 128)
+        self.lin2 = torch.nn.Linear(128, 128)
+        self.lin3 = torch.nn.Linear(128, num_classes)
+
+    def forward(self, data):
+        sa0_out = (data.x, data.pos, data.batch)
+        sa1_out = self.sa1_module(*sa0_out)
+        sa2_out = self.sa2_module(*sa1_out)
+        sa3_out = self.sa3_module(*sa2_out)
+
+        fp3_out = self.fp3_module(*sa3_out, *sa2_out)
+        fp2_out = self.fp2_module(*fp3_out, *sa1_out)
+        x, _, _ = self.fp1_module(*fp2_out, *sa0_out)
+
+        return self.mlp(x).log_softmax(dim=-1)
+
+device = torch.device('cpu')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Net(train_dataset.get_n_classes()).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+def train():
+    model.train()
+
+    total_loss = correct_nodes = total_nodes = 0
+    for i, data in enumerate(train_loader):
+        data = data.to(device)
+        optimizer.zero_grad()
+        out = model(data)
+        print(out)
+        # Add weights to each class
+        loss = torch.nn.CrossEntropyLoss(out, data.y, weight=loss_w)
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+        correct_nodes += out.argmax(dim=1).eq(data.y).sum().item()
+        total_nodes += data.num_nodes
+
+        if (i + 1) % 10 == 0:
+            print(f'[{i+1}/{len(train_loader)}] Loss: {total_loss / 10:.4f} '
+                  f'Train Acc: {correct_nodes / total_nodes:.4f}')
+            total_loss = correct_nodes = total_nodes = 0
+
+
+@torch.no_grad()
+def test(loader):
+    model.eval()
+
+    ious, categories = [], []
+    y_map = torch.empty(loader.dataset.get_n_classes(), device=device).long()
+    for data in loader:
+        data = data.to(device)
+        outs = model(data)
+
+        sizes = (data.ptr[1:] - data.ptr[:-1]).tolist()
+        for out, y, category in zip(outs.split(sizes), data.y.split(sizes),
+                                    data.category.tolist()):
+            category = list(ShapeNet.seg_classes.keys())[category]
+            part = ShapeNet.seg_classes[category]
+            part = torch.tensor(part, device=device)
+
+            y_map[part] = torch.arange(part.size(0), device=device)
+
+            iou = jaccard_index(out[:, part].argmax(dim=-1), y_map[y],
+                                num_classes=part.size(0), absent_score=1.0)
+            ious.append(iou)
+
+        categories.append(data.category)
+
+    iou = torch.tensor(ious, device=device)
+    category = torch.cat(categories, dim=0)
+
+    mean_iou = scatter(iou, category, reduce='mean')  # Per-category IoU.
+    return float(mean_iou.mean())  # Global IoU.
+
+
+# for epoch in range(1, 31):
+#     train()
+#     iou = test(test_loader)
+#     print(f'Epoch: {epoch:02d}, Test IoU: {iou:.4f}')
+
+# test script
+for epoch in range(1, 2):
+    train()
+    print(f'Epoch: {epoch:02d}')
