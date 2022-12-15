66266c8679c62cf724202f7d81229fd41557659b
yanghou2000 <a13008878185@gmail.com>
2022-12-15 00:15:32 +0000

    add iou in test func & clean up dependencies

--- PointNet2_segmentation.py
+++ PointNet2_segmentation.py
@@ -1,4 +1,5 @@
 import os
+import time
 import yaml
 
 import numpy as np
@@ -6,19 +7,24 @@ import torch
 import torch.nn.functional as F
 from Modules import GlobalSAModule, SAModule
 from torch_scatter import scatter
-from torchmetrics.functional import jaccard_index
 
 import torch_geometric.transforms as T
 from Semantic_Dataloader import SemanticKittiGraph
 from torch_geometric.loader import DataLoader
-from torch_geometric.nn import MLP, knn_interpolate
-from torch_geometric.data import Dataset, Data
 
+from torch_geometric.data import Dataset, Data
+from torch.utils.tensorboard import SummaryWriter
+
+from utils import tboard, utils
+from model.PointNet2 import Net
+
+# define the gpu index to train
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 data_path = '/Volumes/scratchdata/kitti/dataset/'
-# DATA_path = '/home/yanghou/project/Panoptic-Segmentation/semantic-kitti.yaml'
-DATA_path = './semantic-kitti.yaml'
-save_path = '/home/yanghou/project/Panoptic-Segmentation/run'
+# DATA_path = '/home/yanghou/project/Panoptic-Segmentation/semantic-kitti.yaml' #
+DATA_path = './semantic-kitti.yaml' # for running in docker
+save_path = './run'
 
 testing_squences = ['00']
 
@@ -35,66 +41,14 @@ test_dataset = SemanticKittiGraph(dataset_dir='/Volumes/scratchdata/kitti/datase
                                 DATA_dir=DATA_path)
 
 torch.manual_seed(42)
-train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=4)
-test_loaer = DataLoader(test_dataset, batch_size=2, shuffle=False, num_workers=4)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
 
 # add loss weights beforehand
 loss_w = train_dataset.map_loss_weight()
 
-class FPModule(torch.nn.Module):
-    def __init__(self, k, nn):
-        super().__init__()
-        self.k = k
-        self.nn = nn
-
-    def forward(self, x, pos, batch, x_skip, pos_skip, batch_skip):
-        x = knn_interpolate(x, pos, pos_skip, batch, batch_skip, k=self.k)
-        if x_skip is not None:
-            x = torch.cat([x, x_skip], dim=1)
-        x = self.nn(x)
-        return x, pos_skip, batch_skip
-
-
-class Net(torch.nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-
-        # Input channels account for both `pos` and node features.
-        """self.sa1_module = SAModule(0.2, 0.2, MLP([3 + 3, 64, 64, 128])) try reducing redundant elements
-        self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
-        self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
-        """
-        self.sa1_module = SAModule(0.2, 0.2, MLP([3, 64, 64, 128]))
-        self.sa2_module = SAModule(0.25, 0.4, MLP([128 + 3, 128, 128, 256]))
-        self.sa3_module = GlobalSAModule(MLP([256 + 3, 256, 512, 1024]))
-
-        self.fp3_module = FPModule(1, MLP([1024 + 256, 256, 256]))
-        self.fp2_module = FPModule(3, MLP([256 + 128, 256, 128]))
-        self.fp1_module = FPModule(3, MLP([128, 128, 128, 128]))
-
-        self.mlp = MLP([128, 128, 128, num_classes], dropout=0.5, norm=None)
-
-        self.lin1 = torch.nn.Linear(128, 128)
-        self.lin2 = torch.nn.Linear(128, 128)
-        self.lin3 = torch.nn.Linear(128, num_classes)
-
-    def forward(self, data):
-        # print('y', type(data.y), data.y.size())
-        # print('pos', type(data.pos), data.pos.size())
-        # print('batch', type(data.batch), data.batch.size(), data.batch)
-        """sa0_out = (data.pos, data.pos, data.batch) try reducing redundant elements
-        """
-        sa0_out = (None, data.pos, data.batch)
-        sa1_out = self.sa1_module(*sa0_out)
-        sa2_out = self.sa2_module(*sa1_out)
-        sa3_out = self.sa3_module(*sa2_out)
-
-        fp3_out = self.fp3_module(*sa3_out, *sa2_out)
-        fp2_out = self.fp2_module(*fp3_out, *sa1_out)
-        x, _, _ = self.fp1_module(*fp2_out, *sa0_out)
-
-        # log_softmax
-        return self.mlp(x).log_softmax(dim=-1)
+# create a SummaryWriter to write logs to a log directory
+writer = SummaryWriter('logs')
 
 # device = torch.device('cpu') # only for debugging
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -102,6 +56,7 @@ model = Net(train_dataset.get_n_classes()).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 nloss = torch.nn.NLLLoss(weight=loss_w).to(device)
 
+# TODO: ignore class label 0
 def train():
     model.train()
 
@@ -110,64 +65,76 @@ def train():
         data = data.to(device)
         optimizer.zero_grad()
         out = model(data)
-        print(out, out.size())
         # negative log likelihood loss. Input should be log-softmax
-        print(type(data.y))
         loss = nloss(out, data.y.cuda())
         loss.backward()
         optimizer.step()
         total_loss += loss.item()
         correct_nodes += out.argmax(dim=1).eq(data.y).sum().item()
         total_nodes += data.num_nodes
+        print('Loss: ', loss)
 
         if (i + 1) % 10 == 0:
             print(f'[{i+1}/{len(train_loader)}] Loss: {total_loss / 10:.4f} '
                   f'Train Acc: {correct_nodes / total_nodes:.4f}')
             total_loss = correct_nodes = total_nodes = 0
+        # Log a scalar value (scalar summary)
+    loss = total_loss / i
+    return loss
+        
 
 
-
-
+# output iou and loss
 @torch.no_grad()
-def test(loader):
+def test(loader, model):
+
     model.eval()
 
-    ious, categories = [], []
+    ious, mious = [], []
     y_map = torch.empty(loader.dataset.get_n_classes(), device=device).long()
+    data_category = list(range(20))
     for data in loader:
         data = data.to(device)
         outs = model(data)
 
         sizes = (data.ptr[1:] - data.ptr[:-1]).tolist()
-        for out, y, category in zip(outs.split(sizes), data.y.split(sizes),
-                                    data.category.tolist()):
-            category = list(ShapeNet.seg_classes.keys())[category]
-            part = ShapeNet.seg_classes[category]
-            part = torch.tensor(part, device=device)
+        for out, y in zip(outs.split(sizes), data.y.split(sizes)):
 
-            y_map[part] = torch.arange(part.size(0), device=device)
+            print('out.size: ', out.size(), 'y.size: ', y.size())
+            iou = utils.calc_iou(out.argmax(dim=-1), y, num_classes=20)
 
-            iou = jaccard_index(out[:, part].argmax(dim=-1), y_map[y],
-                                num_classes=part.size(0), absent_score=1.0)
+            miou = utils.calc_miou(iou, 20)
+
+            print('iou: ', iou, 'miou: ', miou)
             ious.append(iou)
+            mious.append(miou)
 
-        categories.append(data.category)
+        ious_avr = torch.sum(ious) / len(ious)
+        miou_avr = torch.sum(mious) / len(miou)
 
-    iou = torch.tensor(ious, device=device)
-    category = torch.cat(categories, dim=0)
-
-    mean_iou = scatter(iou, category, reduce='mean')  # Per-category IoU.
-    return float(mean_iou.mean())  # Global IoU.
+        return ious_avr, miou_avr
 
 
 # for epoch in range(1, 31):
-#     train()
-#     iou = test(test_loader)
+#     loss = train()
+#     ious, miou = test(test_loader)
+#     writer.add_scalar('train/loss', loss, epoch)
+#     writer.add_scalar('train/iou', miou, epoch)
+#     tboard.add_iou(writer, ious, epoch)
+#     state = {'net':model.state_dict(), 'epoch':epoch}
+#     torch.save(state, f'{save_path}/Epoch_{epoch}_{time.strftime("%Y%m%d_%H%M%S")}.pth')
 #     print(f'Epoch: {epoch:02d}, Test IoU: {iou:.4f}')
 
 # test script
 for epoch in range(1, 2):
-    train()
+    loss = train()
     print(f'Epoch: {epoch:02d}')
     state = {'net':model.state_dict(), 'epoch':epoch}
-    torch.save(state, f'{save_path}/Epoch_{epoch}.pth')
+    torch.save(state, f'{save_path}/Epoch_{epoch}_{time.strftime("%Y%m%d_%H%M%S")}.pth')
+
+
+
+# # debugging test()
+# state_dict = torch.load('./run/Epoch_3_20221214_210321.pth')['net']
+# model.load_state_dict(state_dict)
+# test(test_loader, model)
